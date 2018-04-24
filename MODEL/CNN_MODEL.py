7343554abfdb5743b4fb0a4791b1a0d6ad6fb064
ffb <3197235088@qq.com>
2018-04-24 22:15:34 +0800

    98% model

--- MODEL/CNN_MODEL.py
+++ MODEL/CNN_MODEL.py
@@ -21,6 +21,7 @@ class DATA():
                 img_arr = np.array(img)
 
                 if (img_arr.shape == (32, 32)):
+                    img_arr = img_arr.reshape((32, 32, 1))
                     X[LEN] = img_arr
                     Y[LEN] = [0] * 5
 
@@ -37,7 +38,7 @@ class DATA():
         return X, Y
 
     
-    def get_train_data(self, size=19500):
+    def get_train_data(self, size=20000):
 
         BRANCH_NUM = int(size/(cf.BRANCH_SIZE * 5))
 
@@ -78,12 +79,13 @@ class DATA():
         Y = Y[: LEN]
 
         X = np.array(X)
+        X = X.reshape((-1,32,32,1))
         Y = np.array(Y)
         return X, Y
     
 
 '''
-测试DATA类
+#测试DATA类
 data = DATA()
 X, Y = data.get_train_data(10000)
 
@@ -91,3 +93,141 @@ x_test, y_test = data.get_test_data(450)
 print(x_test.shape)
 print(y_test.shape)
 '''
+
+class MODEL():
+    def __init__(self):
+        self.X = tf.placeholder(dtype=tf.float32, shape=[None, 32, 32, 1], name='X_input')
+        self.Y = tf.placeholder(dtype=tf.float32, shape=[None, 5])
+        self.data = DATA()
+
+
+    def assemble_graph(self, w_alpha = 0.01, b_alpha=0.01):
+        #第一层
+        #10个3*3*3的卷积核
+        w_c1 = tf.Variable(w_alpha*tf.random_normal([2, 2, 1, 16]), dtype=tf.float32)
+        b_c1 = tf.Variable(b_alpha*tf.random_normal([16]), dtype=tf.float32)
+
+        conv1 = tf.nn.tanh(tf.nn.bias_add(tf.nn.conv2d(self.X, w_c1, strides=[1, 1, 1, 1], padding='SAME'), b_c1))
+        conv1 = tf.nn.max_pool(conv1, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        #shape为(size, 16, 16, 16)
+
+        
+
+        
+        #第二层
+        w_c2 = tf.Variable(w_alpha*tf.random_normal([2, 2, 16, 32]), dtype=tf.float32)
+        b_c2 = tf.Variable(b_alpha*tf.random_normal([32]), dtype=tf.float32)
+
+        conv2 = tf.nn.tanh(tf.nn.bias_add(tf.nn.conv2d(conv1, w_c2, strides=[1, 1, 1, 1], padding='SAME'), b_c2))
+        conv2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        #shape为(size, 8, 8, 32)
+        
+        
+        
+        #第三层
+        w_c3 = tf.Variable(w_alpha*tf.random_normal([2, 2, 32, 64]), dtype=tf.float32)
+        b_c3 = tf.Variable(b_alpha*tf.random_normal([64]), dtype=tf.float32)
+
+        conv3 = tf.nn.tanh(tf.nn.bias_add(tf.nn.conv2d(conv2, w_c3, strides=[1, 1, 1, 1], padding='SAME'), b_c3))
+        conv3 = tf.nn.max_pool(conv3, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
+        #shape为(size, 4, 4, 64)
+
+        
+        #全连接层
+        cnn_out = tf.reshape(conv3, [-1, 4 * 4 * 64])
+        w_out = tf.Variable(w_alpha*tf.random_normal([4 * 4 * 64, 5]), dtype=tf.float32)
+        b_out = tf.Variable(w_alpha*tf.random_normal([5]), dtype=tf.float32)
+
+        Y_p =  tf.add(tf.matmul(cnn_out, w_out), b_out)
+        #shape为(size, 5)
+        
+        '''
+        #测试用代码
+        x,y = self.data.get_train_data()
+        with tf.Session() as sess:
+            sess.run(tf.global_variables_initializer())
+            out = sess.run([Y_p], feed_dict={self.X:x[0]})
+            print(np.array(out).shape)
+        '''
+        return Y_p
+
+    
+    def get_acc(self, Y_p, Y):
+        Y_p = Y_p[0]
+        Y_p = np.array(Y_p)
+
+
+        size, num = Y_p.shape
+        print(size)
+        print(num)
+        
+        ACC = 0
+        SUM = size
+
+        for k in range(size):
+            poi = 0
+            for i in range(num):
+                if (Y_p[k][poi] < Y_p[k][i]):
+                    poi = i
+                
+            if (Y[k][poi] == 1):
+                ACC = ACC + 1
+
+        return ACC/SUM 
+        
+        return 0
+    
+    def train(self, size = 20000, w_alpha = 0.01, b_alpha=0.01):
+        #读入数据
+        X, Y = self.data.get_train_data(size)
+        x_test, y_test = self.data.get_test_data()
+
+        
+        
+        #获取神经网络输出与预测值
+        BRANCH_NUM = len(X)
+        network_output = self.assemble_graph(w_alpha, b_alpha)
+        Y_p = tf.nn.softmax(network_output, name='Y_p')
+
+
+
+        #计算loss
+        loss = tf.nn.softmax_cross_entropy_with_logits(logits=network_output, labels=self.Y)
+        loss = tf.reduce_mean(loss)
+
+        #设置op
+        op = tf.train.AdamOptimizer(learning_rate=0.0001).minimize(loss)
+
+
+        saver = tf.train.Saver()
+
+        with tf.Session() as sess:
+            #sess.run(tf.global_variables_initializer())
+            saver.restore(sess, cf.MODEL_PATH + 'CNN.model-98')
+
+            step = 0
+            print('begin')
+            
+            while True: 
+                
+                _, L = sess.run([op,loss], feed_dict={self.X:X[step % BRANCH_NUM], self.Y:Y[step % BRANCH_NUM]})
+                
+                if step % 20 == 0:
+                    print(str(step) + '  ' + str(L))
+                      
+                
+                if step % 100 == 0:
+                    y_p = sess.run([Y_p], feed_dict={self.X:x_test})
+                    acc = self.get_acc(y_p, y_test)
+                    print(str(step) + ' ' + str(acc))
+                
+                
+                if step % 500 == 0:
+                    saver.save(sess, cf.MODEL_PATH + 'CNN.model', global_step=step)
+                
+                step = step + 1
+        
+
+
+model = MODEL()
+model.train()
--- MODEL/config.py
+++ MODEL/config.py
@@ -1,3 +1,3 @@
 DATA_PATH = '/home/ffb/Workspace/Python-srf/手写字识别/数据集/'
-
+MODEL_PATH = '/home/ffb/Workspace/Python-srf/手写字识别/Hand-writing-recognition/MODEL/MODEL/'
 BRANCH_SIZE = 1000
